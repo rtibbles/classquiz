ead887d92f7a41404da1ef90bd52404bb6a82e7b
Richard Tibbles <richard.tibbles@gmail.com>
2016-10-05 13:23:21 -0700

    First pass implementation of socket based class quizzing.

---
+++ server.js
@@ -0,0 +1,57 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var classNameSpace = io.of('/class-namespace');
+
+var adminNameSpace = io.of('/admin-namespace');
+
+var quizStarted = false;
+
+var users = {};
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname});
+});
+
+app.get('/admin', function(req, res){
+  res.sendFile('admin.html', {root: __dirname});
+});
+
+classNameSpace.on('connection', function(socket){
+  var user = {
+  	id: socket.id
+  };
+  
+  socket.on('response', function(response){
+    adminNameSpace.emit('response', {
+      response: response,
+      user_id: user.id
+    });
+  });
+  socket.on('email', function(email){
+  	user.email = email;
+  	users[email] = user;
+  	adminNameSpace.emit('email', user);
+  	if (quizStarted) {
+	  socket.emit('new question');
+  	}
+  });
+  socket.on('disconnect', function(){
+    adminNameSpace.emit('disconnected', user);
+  })
+});
+
+adminNameSpace.on('connection', function(socket){
+
+  socket.emit('users', users);
+  
+  socket.on('new question', function(response){
+    classNameSpace.emit('new question');
+    quizStarted = true;
+  });
+});
+
+http.listen(8008, function(){
+  console.log('listening on *:8008');
+});
